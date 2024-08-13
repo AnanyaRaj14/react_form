1a592dcdb3653be91ad0af1f3ca35763cf62c206
Ananya Raj <ananyam748@gmail.com>
2024-08-13 21:16:37 +0530

    some issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,63 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [password, setPass] = useState("");
+  const [conPass, setconPass] = useState("");
+
+
+
+  const submitData = () => {
+    if(!name || !mail || !password || !conPass){
+      alert("kindly fill name, email, and both password field!!");
+      return;
+    }
+    if(password != conPass){
+      alert("both password do not matches!!")
+      return;
+    }
+
+
+    console.log("welcome");
+    // console.log(`Welcome ${name}, your email id is ${mail}`);
+
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Registration Form</h1>
+     <input 
+        type="text" 
+        placeholder='Enter your name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input 
+        type="email" 
+        placeholder='Enter your email'
+        value={mail}
+        onChange={(e) => setMail(e.target.value)}
+      />
+      <br />
+      <input 
+        type="password" 
+        placeholder='Enter password'
+        value={password}
+        onChange={(e) => setPass(e.target.value)}
+      />
+      <br />
+      <input 
+        type="password" 
+        placeholder='Confirm Password'
+        value={conPass}
+        onChange={(e) => setconPass(e.target.value)}
+      />
+      <br />
+     <button onClick={submitData}>Submit</button>
     </>
   )
 }
